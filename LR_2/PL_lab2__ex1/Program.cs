646a9b413faf96440c03050afa32d44c6e219967
Myaroshev <63793985+Myaroshev@users.noreply.github.com>
2024-10-25 13:15:35 +0500

    Add files via upload

---
+++ LR_2/PL_lab2__ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PL_lab2__ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 original = new Class1(3,11,6);
+
+            Class1 copy = new Class1(original);
+
+            Console.WriteLine(copy.ToString());
+
+            int minLastDigit = original.min_p();
+            Console.WriteLine($"Минимальная из последних цифр: {minLastDigit}");
+            
+
+            //дочерний
+            Console.WriteLine();
+            Triangle triangle = new Triangle(original.x, original.y, original.z);
+            Console.WriteLine(triangle);
+        }
+    }
+}
