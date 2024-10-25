db1c1f6f5e53580a037363f3a4cdc2581ecfc7a4
Myaroshev <63793985+Myaroshev@users.noreply.github.com>
2024-10-25 13:19:29 +0500

    Add files via upload

---
+++ PLL_lab2_ex1/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLL_lab2_ex1
+{
+    internal class Class1
+    {
+        protected internal int x;
+        protected internal int y;
+        protected internal int z;
+
+        //конструктор инициализации
+        public Class1(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        //конструктор копирования
+        public Class1(Class1 temp)
+        {
+            this.x = temp.x;
+            this.y = temp.y;
+            this.z = temp.z;
+        }
+
+        public int min_p()
+        {
+            int digit_1 = Math.Abs(x) % 10;
+            int digit_2 = Math.Abs(y) % 10;
+            int digit_3 = Math.Abs(z) % 10;
+
+            int min_digit = digit_1;
+
+            if (digit_2 < min_digit)
+            {
+                min_digit = digit_2;
+            }
+            else if (digit_3 < min_digit)
+            {
+                min_digit = digit_3;
+            }
+
+            return min_digit;
+        }
+
+        //перегрузка
+        public override string ToString()
+        {
+            return $"x: {x}\ny: {y}\nz: {z}";
+        }
+    }
+}
---
+++ PLL_lab2_ex1/Triangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLL_lab2_ex1
+{
+    internal class Triangle : Class1
+    {
+        public Triangle(int x, int y, int z) : base(x, y, z) { }
+        //поля - длины сторон треугольника
+
+        //периметр
+        public int Perimeter()
+        {
+            return x + y + z;
+        }
+
+        //площадь
+        public double Area()
+        {
+            double semi_perimeter = Perimeter() / 2;
+
+            double area = Math.Sqrt(semi_perimeter * ((semi_perimeter - x) * (semi_perimeter - y) * (semi_perimeter - z)));
+
+            return area;
+        }
+
+        //существование
+        public bool isExists()
+        {
+            return (x + y > z && x + z > y && y + z > x);
+        }
+
+        //перегрузка
+        public override string ToString()
+        {
+            return base.ToString() + $"\nПлощадь: {Area()}\nПериметр: {Perimeter()}\nСуществует: {isExists()}";
+        }
+    }
+}
